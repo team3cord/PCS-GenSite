f00c103c9efe981ef420e75de422b5822a803633
MattCordeiro <matthew3cord@gmail.com>
2015-01-27 13:30:40 -0800

    Updated layout.jade-no error/inclued sitegen package- success/ creating correct jade layout.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var postbatch = require('sitegen');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'N0d3 M4dN3SS!' });
 });
 
+postbatch.consoleMe('bill');
 module.exports = router;
