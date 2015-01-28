0f78d98b86c6211c257d3f109e1cf71f2ea3e6c0
MattCordeiro <matthew3cord@gmail.com>
2015-01-27 20:02:06 -0800

    Added jade posts & sample md post for testing.

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'N0d3 M4dN3SS!' });
+  res.render('index', { title: 'N0d3 M4dN3SS!', proccessedPosts: fileCollector});
 });
+// path for postbatch  
+var postPath = './public/posts';
+postbatch.postBatch(postPath);
+console.log(fileCollector);
 
 postbatch.consoleMe('bill');
+
 module.exports = router;
