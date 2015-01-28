0e72897241178444c5b9d2b28650dd479c634e89
MattCordeiro <matthew3cord@gmail.com>
2015-01-28 11:12:42 -0800

    Cleaned up postBatch function implementation/ still issues with jade rendering.

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,17 @@ var express = require('express');
 var postbatch = require('SiteGen');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'N0d3 M4dN3SS!', proccessedPosts: postbatch.fileCollector});
-});
 // path for postbatch  
 var postPath = './public/posts';
-var fileCollector = [];
-postbatch.postBatch(postPath,fileCollector);
-console.log(postbatch.fileCollector);
+var jadeArray = postbatch.postBatch(postPath);
+console.log(jadeArray);
 
 postbatch.consoleMe('bill');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'N0d3 M4dN3SS!', proccessedPosts: jadeArray});
+});
+
 
 module.exports = router;
